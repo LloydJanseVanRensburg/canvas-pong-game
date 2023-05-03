c228d6a79683e02090036760f412a8f62ee6ce68
Lloyd JvR <thefullstackjunkie@gmail.com>
2023-05-03 22:31:32 +0200

    add: basic pong game completed

--- app.ts
+++ app.ts
@@ -1,41 +1,199 @@
 const canvas = document.querySelector('#myCanvas')! as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
-class Player {
-    x: number;
-    y: number;
+type Mixed = Player | Ball;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+class Game {
+    req: number;
+    keyPlayer1: { [key: string]: boolean };
+    keyPlayer2: { [key: string]: boolean };
+    players: Player[];
+    ball: Ball;
+
+    constructor() {
+        this.req = -1;
+        this.keyPlayer1 = {
+            ArrowLeft: false,
+            ArrowRight: false,
+            ArrowUp: false,
+            ArrowDown: false,
+        };
+        this.keyPlayer2 = {
+            KeyA: false,
+            KeyD: false,
+            KeyW: false,
+            KeyS: false,
+        };
+        this.players = [
+            new Player(50, 50, 5, 15, 100, 'blue'),
+            new Player(550, 50, 5, 15, 100, 'red'),
+        ];
+        this.ball = new Ball(200, 200, 10, 10, 5, 5, 'white');
+
+        document.addEventListener('keydown', (event) => {
+            if (event.code in this.keyPlayer1) {
+                this.keyPlayer1[event.code] = true;
+            }
+            if (event.code in this.keyPlayer2) {
+                this.keyPlayer2[event.code] = true;
+            }
+        });
+        document.addEventListener('keyup', (event) => {
+            if (event.code in this.keyPlayer1) {
+                this.keyPlayer1[event.code] = false;
+            }
+            if (event.code in this.keyPlayer2) {
+                this.keyPlayer2[event.code] = false;
+            }
+        });
+    }
+
+    start() {
+        this.req = requestAnimationFrame(this.draw.bind(this));
     }
 
     draw() {
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(this.x, this.y, 100, 100);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.font = '24px sans-serif';
-        ctx.textAlign = 'center';
-        ctx.fillStyle = 'red';
-        let output = `Pos {x: ${this.x},y: ${this.y}}`;
-        ctx.fillText(output, 300, 30);
+        this.move();
 
-        ctx.beginPath();
-        ctx.fillStyle = 'blue';
-        ctx.moveTo(50, 200);
-        ctx.lineTo(150, 250);
-        ctx.lineTo(150, 150);
-        ctx.fill();
+        this.checkCollision(this.players[0], this.players[1]);
 
-        ctx.beginPath();
-        ctx.arc(150, 300, 50, 0, 2 * Math.PI);
-        ctx.strokeStyle = 'yellow';
-        ctx.fillStyle = 'green';
-        ctx.fill();
-        ctx.stroke();
+        this.players.forEach((player) => player.draw());
+        this.ball.draw();
+
+        this.req = requestAnimationFrame(this.draw.bind(this));
+    }
+
+    checkCollision(obj1: Mixed, obj2: Mixed) {
+        return (
+            obj1.x < obj2.x + obj2.width &&
+            obj1.x + obj1.width > obj2.x &&
+            obj1.y < obj2.y + obj2.height &&
+            obj1.y + obj1.height > obj2.y
+        );
+    }
+
+    move() {
+        this.ball.x += this.ball.speedX;
+        this.ball.y += this.ball.speedY;
+
+        if (this.ball.x < 0 || this.ball.x > canvas.width) {
+            this.ball.speedX *= -1;
+        }
+        if (this.ball.y < 0 || this.ball.y > canvas.height) {
+            this.ball.speedY *= -1;
+        }
+        if (this.checkCollision(this.ball, this.players[0])) {
+            this.ball.speedX *= -1;
+            const centerPaddleY =
+                (this.players[0].y + this.players[0].height) / 2;
+            const ballY = this.ball.y;
+
+            if (centerPaddleY < ballY) {
+                this.ball.speedY = Math.abs(this.ball.speedY);
+            } else {
+                this.ball.speedY = Math.abs(this.ball.speedY) * -1;
+            }
+        }
+
+        if (this.checkCollision(this.ball, this.players[1])) {
+            this.ball.speedX *= -1;
+            const centerPaddleY =
+                (this.players[1].y + this.players[1].height) / 2;
+            const ballY = this.ball.y;
+
+            if (centerPaddleY < ballY) {
+                this.ball.speedY = Math.abs(this.ball.speedY);
+            } else {
+                this.ball.speedY = Math.abs(this.ball.speedY) * -1;
+            }
+        }
+
+        if (this.keyPlayer1.ArrowRight) {
+            this.players[0].x += this.players[0].speed;
+        } else if (this.keyPlayer1.ArrowLeft) {
+            this.players[0].x -= this.players[0].speed;
+        } else if (this.keyPlayer1.ArrowUp) {
+            this.players[0].y -= this.players[0].speed;
+        } else if (this.keyPlayer1.ArrowDown) {
+            this.players[0].y += this.players[0].speed;
+        }
+
+        if (this.keyPlayer2.KeyD) {
+            this.players[1].x += this.players[1].speed;
+        } else if (this.keyPlayer2.KeyA) {
+            this.players[1].x -= this.players[1].speed;
+        } else if (this.keyPlayer2.KeyW) {
+            this.players[1].y -= this.players[1].speed;
+        } else if (this.keyPlayer2.KeyS) {
+            this.players[1].y += this.players[1].speed;
+        }
     }
 }
 
-const player = new Player(50, 50);
+class Player {
+    x: number;
+    y: number;
+    speed: number;
+    width: number;
+    height: number;
+    color: string;
 
-player.draw();
+    constructor(
+        x: number,
+        y: number,
+        speed: number,
+        width: number,
+        height: number,
+        color: string
+    ) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Ball {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    speedX: number;
+    speedY: number;
+    color: string;
+
+    constructor(
+        x: number,
+        y: number,
+        width: number,
+        height: number,
+        speedX: number,
+        speedY: number,
+        color: string
+    ) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const game = new Game();
+game.start();
